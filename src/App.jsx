d72e517a4d80b9e67d49474713cf692dba2c6222
thasipablo <thasipablo@gmail.com>
2023-03-13 11:04:27 -0400

    hero heading added

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
-          <Experience />
+          <Hero />
         </div>
         <About />
         <Experience />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,10 +9,20 @@ const Hero = () => {
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915eff]">
-            Lorem ipsum dolor sit amet.
-          </div>
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className="text-[#915eff]">Pablo</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop hire available and scalable softwares
+            <br className="sm:block hidden" /> do better UX designs
+          </p>
+        </div>
+
+        <ComputersCanvas />
       </div>
     </section>
   );
