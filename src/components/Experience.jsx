a177938f911511c0ebd2c5219bf243b923c05c4b
thasipablo <thasipablo@gmail.com>
2023-03-17 04:23:06 -0400

    experiences tree completed

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -12,20 +12,35 @@ import { SectionWrapper } from "../hoc";
 import { textVariant } from "../utils/motion";
 
 const ExperienceCard = ({ experience }) => {
-  <VerticalTimelineElement
-    contentStyle={{ background: "#1d1836", color: "#fff" }}
-    contentArrowStyle={{ borderRight: "7px solid #232631" }}
-    date={experience.date}
-    iconStyle={{background: experience.iconBg}}
-    icon={
+  return (
+    <VerticalTimelineElement
+      contentStyle={{ background: "#1d1836", color: "#fff" }}
+      contentArrowStyle={{ borderRight: "7px solid #232631" }}
+      date={experience.date}
+      iconStyle={{ background: experience.iconBg }}
+      icon={
+        <div className="flex justify-center items-center w-full h-full">
+          <img
+            src={experience.icon}
+            alt={experience.company_name}
+            className="w-[60%] h-[60%] object-contain"
+          />
+        </div>
+      }
+    >
       <div>
-        <img src={experience.icon} alt={experience.company_name} className="w-[60%] h-[60%] object-contain"/>
+        <h3 className="text-white text-[24px] font-bold">{experience.title}</h3>
+        <p className="text-secondary font-semibold text-[16px]" style={{margin: 0}}>{experience.company_name}</p>
       </div>
-    }
-    
-  >
-    {experience.date}
-  </VerticalTimelineElement>;
+      <ul className="mt-5 list-disc ml-5 space-y-2">
+        {experience.points.map((point, index) => (
+          <li key={`experience-point-${index}`} className="text-white-100 text-[14px] pl-1 tracking-wider">
+            {point}
+          </li>
+        ))}
+      </ul>
+    </VerticalTimelineElement>
+  );
 };
 
 const Experience = () => {
