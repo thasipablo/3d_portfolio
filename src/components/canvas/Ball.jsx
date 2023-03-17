336846650a554bf57b9830339bbbbe534ad403af
thasipablo <thasipablo@gmail.com>
2023-03-17 12:26:08 -0400

    Ball canvas done

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -15,9 +15,21 @@ const Ball = (props) => {
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25} />
-      <directionalLight position={[0,0,0.05]} />
+      <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronBufferGeometry args={[1,1]} />
+        <icosahedronBufferGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color="#ff8ed"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          map={decal}
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          flatShading
+        />
       </mesh>
     </Float>
   );
